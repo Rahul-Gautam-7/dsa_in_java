8ae3803d0866ca4e3a3cdb9efd2682fb323bcb0b
Rahul-Gautam-7 <rahulgautam7r@gmail.com>
2024-05-27 08:43:22 +0530

    reverse linked lst

---
+++ Reverselinkedlst.java
@@ -0,0 +1,59 @@
+import java.util.LinkedList;
+
+public class Reverselinkedlst {
+
+   static Node head;
+
+    class Node{
+
+        String data;
+        Node next;
+        
+        Node(String data){
+
+            this.data = data;
+            this.next=null;
+            
+        }
+    }
+
+
+    public static void reverselst(LinkedList<Integer> lst){
+
+        if(head == null || head.next == null)
+        {
+            return;
+        }
+
+        Node prev = head;
+        Node curr=head.next;
+
+        while (curr != null) {
+
+            Node nxt = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nxt;
+            
+        }
+        head.next = null;
+        head =prev;
+
+    }
+
+    public static void main(String[] args) {
+        LinkedList<Integer> lst = new LinkedList<>();
+        lst.add(1);
+        lst.add(2);
+        lst.add(3);
+        lst.add(4);
+
+        System.out.println(lst);
+        
+        reverselst(lst);
+        System.out.println(lst);
+        
+    }
+
+    
+}
