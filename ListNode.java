4bf9ce9b24a16e8be31a20501d4b67cf2796f4da
Rahul Gautam <rahulgautam7r@gmail.com>
2024-05-29 08:49:21 +0530

    delete element from linked lst from end

---
+++ ListNode.java
@@ -0,0 +1,91 @@
+
+
+ public class ListNode{
+    int data;
+  ListNode next;
+
+  ListNode(int value)
+  {
+    this.data = value;
+    this.next = null;
+  }
+    
+ }
+
+ 
+    
+  
+
+
+
+
+class DelEement{
+
+    static int length(ListNode head)
+    {
+      ListNode temp = head;
+      int count = 0;
+      while (temp != null) {
+        count++;
+        temp = temp.next;
+      }
+      return count;
+    }
+
+    static void printList(ListNode head)
+    {
+      ListNode ptr = head;
+      while (ptr != null) {
+        System.out.print(ptr.data + " ");
+        ptr = ptr.next;
+      }
+      System.out.println();
+    }
+
+    public static ListNode delElement(ListNode head,int n) {
+
+        if(head.next == null)
+        {
+            return  null;
+        }
+        int size=0;
+        ListNode curr = head;
+        while(curr !=null)
+        {
+            curr=curr.next;
+            size++;
+        }
+        if(n==size)
+        {
+            return head.next;
+        }
+
+        int ios=size-n;
+        ListNode prev=head;
+        int i=1;
+        while(i<ios)
+        {
+            prev = prev.next;
+            i++;
+        }
+        prev.next=prev.next.next;
+        return head;
+    }
+
+    public static void main(String[] args) {
+        
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next=new ListNode(4);
+        head.next.next.next.next=new ListNode(5);
+
+        printList(head);
+        head = delElement(head,3);
+        printList(head);
+    }
+    
+}
+
+
+
