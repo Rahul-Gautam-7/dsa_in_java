cf0ffbe61ee434955672c459954ca6d012ba6a16
rahulgautam7 <rahulgautam7r@gmail.com>
2024-06-03 11:23:33 +0530

    Stack using arraylist

---
+++ StackArrayListLL.java
@@ -0,0 +1,55 @@
+import java.util.ArrayList;
+
+public class StackArrayListLL {
+
+   static class Stack{
+     ArrayList<Integer> list = new ArrayList<>();
+    public  boolean isEmpty()
+    {
+        return list.size()==0;
+
+    }
+    public  void push(int data)
+    {
+        list.add(data);
+    }
+    public  int pop()
+    {
+        if(isEmpty())
+        {
+            return -1;
+        }
+        int top = list.remove(list.size() - 1);
+       
+        return top;
+
+    }
+    public  int peek()
+    {
+        if(isEmpty())
+        {
+            return -1;
+        }
+        return list.get(list.size()-1);
+    }
+   }
+
+
+    public static void main(String[] args) {
+
+        Stack s = new Stack();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
+        
+        while (!s.isEmpty()) {
+
+            System.out.print(s.peek()+" ");
+            s.pop();
+            
+        }
+        
+    }
+    
+}
