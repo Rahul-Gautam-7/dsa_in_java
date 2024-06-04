2257ab9a9dbfaad7334d3ee44dcebf8ad717d340
rahul gautam <rahulgautam7r@gmail.com>
2024-06-04 08:38:36 +0530

    stack collection framework

---
+++ StackCollection.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class StackCollection{
+
+    public static void main(String[] args) {
+        Stack<Integer> s = new Stack<Integer>();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
+
+        while(!s.isEmpty())
+        {
+            System.out.print(s.peek()+" ");
+            s.pop();
+
+        }
+    }
+
+}
