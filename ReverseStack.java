3fa822984a3e7d2f76c4625ff973e74e7a3b1551
rahul gautam <rahulgautam7r@gmail.com>
2024-06-04 10:11:41 +0530

    reverse the stack

--- ReverseStack.java
+++ ReverseStack.java
@@ -1,43 +1,42 @@
 import java.util.*;
+public class ReverseStack {
 
-public class Pushbottomele {
+    public static void pushatbottom(int data,Stack<Integer> s) {
 
-    public static void pushatbottom(int data, Stack<Integer> s)
-    {
         if(s.isEmpty())
         {
             s.push(data);
             return;
         }
-        int top = s.pop();
-        pushatbottom(data, s);
+        int top =s.pop();
+        pushatbottom(data,s);
         s.push(top);
-    }
+        }
+
     public static void reverse(Stack<Integer> s)
     {
         if(s.isEmpty())
         {
             return;
         }
-        int top = s.pop();
+        int top =s.pop();
         reverse(s);
         pushatbottom(top, s);
     }
-
     public static void main(String[] args) {
-        Stack<Integer> s = new Stack<Integer>();
 
+        Stack<Integer> s = new Stack<>();
         s.push(1);
         s.push(2);
         s.push(3);
-
-       reverse(s);
-
+        s.push(4);
+        reverse(s);
         while(!s.isEmpty())
         {
             System.out.print(s.peek()+" ");
             s.pop();
         }
+        
     }
     
 }
